a8f9652cb63525ae054b66391190e67949b103e1
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-08-08 16:01:52 -0600

    1: Creacion de variables de entorno

---
+++ config/mongo.js
@@ -0,0 +1,43 @@
+'use strict'
+
+import mongoose from "mongoose";
+
+export const dbConnection = async () => {
+
+    try {
+
+        mongoose.connection.on('error', () => {
+
+            console.log('MongoDB | Could not be connect to MongoDB')
+            mongoose.disconnect();
+        })
+        mongoose.connection.on('connecting', () => {
+
+            console.log('MongoDB | Try connecting')
+        })
+        mongoose.connection.on('connected', () => {
+
+            console.log('MongoDB | Connected to MongoDB')
+        })
+        mongoose.connection.on('open', () => {
+
+            console.log('MongoDB | Connected to database')
+        })
+        mongoose.connection.on('reconnected', () => {
+
+            console.log('MongoDB | Reconnected to MongoDB')
+        })
+        mongoose.connection.on('disconected', () => {
+
+            console.log('MongoDB | Disconnected')
+        })
+
+        await mongoose.connect(process.env.URI_MONGO, {
+
+            serverSelectionTimeoutMs: 10000
+        })
+    } catch (e) {
+
+        console.log('Database connection failed', e)
+    }
+}
